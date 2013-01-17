79aa240973d3a5c7d71ca6d91f2df2cff5f49985
Dmitry Kurinskiy <name.alari@gmail.com>
2013-01-17 11:19:38 +0400

    tests passed

---
+++ test/unit/infra/file/storage/FileDomainSpec.groovy
@@ -0,0 +1,14 @@
+package infra.file.storage
+
+import grails.test.mixin.TestFor
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(FileDomain)
+class FileDomainSpec {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
